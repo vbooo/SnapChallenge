c83d4d5cb3db81c94ea56613e41a6b127f80d6b6
DESKTOP-U66AR6L\fabie <fabien.bouvet78@gmail.com>
2018-09-26 22:55:46 +0200

    test

--- app/src/main/java/mypromotion/vboo/com/snapchallenge/model/Challenge.kt
+++ app/src/main/java/mypromotion/vboo/com/snapchallenge/model/Challenge.kt
@@ -6,20 +6,18 @@ package mypromotion.vboo.com.snapchallenge.model
 class Challenge {
 
     private var id: Int? = null
-    private var earnPoints: Int? = null
     private var objectiveTime: Int? = null
     private var releasedTime: Int? = null
     private var idAuthor: Int? = null
     private var succeed: Boolean? = null
     private var idTypeChallenge: Int? = null
 
-    constructor(id: Int?, earnPoints: Int?, objectiveTime: Int?, releasedTime: Int?, idAuthor: Int?, succeed: Boolean?) {
+    constructor(id: Int?, objectiveTime: Int?, releasedTime: Int?, idAuthor: Int?, succeed: Boolean?, idTypeChallenge: Int?) {
         this.id = id
-        this.earnPoints = earnPoints
         this.objectiveTime = objectiveTime
         this.releasedTime = releasedTime
         this.idAuthor = idAuthor
         this.succeed = succeed
+        this.idTypeChallenge = idTypeChallenge
     }
-
 }
